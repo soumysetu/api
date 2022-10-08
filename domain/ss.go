2290496649505feda305c24756a0089cffd11dbb
soumysetu <105278271+soumysetu@users.noreply.github.com>
2022-10-08 22:28:54 +0530

    Create ss.go

---
+++ domain/ss.go
@@ -0,0 +1,15 @@
+type CustomerRepositoryStub struct {
+	customers []customer
+}
+
+func (s CustomerRepositoryStub) findall() ([]customer, error) {
+	return s.customers, nil
+}
+
+func NewCustomerRepositorystub() CustomerRepositoryStub {
+	customers := []customer{
+		{id: "565", name: "fsgsfde", city: "bjbjh", zipcode: "54656", status: "65465jhcf"},
+		{id: "987", name: "654gefe", city: "knjkj", zipcode: "748798", status: ".ljhgbju"},
+	}
+	return CustomerRepositoryStub{customers}
+}
